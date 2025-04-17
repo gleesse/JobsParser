c56378cca9b3a558edb7bd064b25e74e6cf03fa1
gleesse <gl.pvn12@gmail.com>
2025-04-17 12:57:23 +0200

    Added message field to exit command to later use in logging process

--- JobsParser.AutoApplyService/Commands/ActionCommands/ExitCommand.cs
+++ JobsParser.AutoApplyService/Commands/ActionCommands/ExitCommand.cs
@@ -2,12 +2,16 @@ using Microsoft.Playwright;
 
 namespace JobsParser.AutoApplyService.Commands.ActionCommands
 {
-    public class ExitCommand(bool success, ILogger<ExitCommand> logger) : Command(logger)
+    public class ExitCommand(bool success, string? message, ILogger<ExitCommand> logger) : Command(logger)
     {
         public override Task ExecuteAsync(IPage page, CommandContext context)
         {
-            _logger.LogInformation("Exiting workflow with success: {Success}", success);
+            _logger.LogInformation("Executing exit command with success: {Success}", success);
             context.SetVariable("WorkflowFinishedSuccessfully", success);
+
+            if(!string.IsNullOrEmpty(message))
+                context.SetVariable("WorkflowFinishedMessage", message);
+
             return Task.CompletedTask;
         }
     }
